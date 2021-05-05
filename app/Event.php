09caaa9d1ab59ed92c94e29ce14db36d761de020
Chris <chrisliu0416@gmail.com>
2021-05-05 16:08:19 -0700

    fix bug with carbon parse datetime 00:00 in Event Model

--- app/Event.php
+++ app/Event.php
@@ -13,7 +13,7 @@ class Event extends Model
     public function getStartAttribute($value)
     {
         $dateStart = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y-m-d');
-        $timeStart = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i:s');
+        $timeStart = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i');
 
         return $this->start = ($timeStart == '00:00:00' ? $dateStart : $value);
     }
@@ -21,7 +21,7 @@ class Event extends Model
     public function getEndAttribute($value)
     {
         $dateEnd = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y-m-d');
-        $timeEnd = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i:s');
+        $timeEnd = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i');
 
         return $this->end = ($timeEnd == '00:00:00' ? $dateEnd : $value);
     }
