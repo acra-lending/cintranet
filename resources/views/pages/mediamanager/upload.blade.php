7ed9d6d2f905e74947965e33e6aba8b26392b275
Chris <chrisliu0416@gmail.com>
2021-08-23 10:37:18 -0700

    fix bug in file upload (forgot comma in array)

--- resources/views/pages/mediamanager/upload.blade.php
+++ resources/views/pages/mediamanager/upload.blade.php
@@ -135,7 +135,7 @@
                                                                     ],
                                                                     'Underwriting' => [
                                                                         'uwCompliance'          => 'Compliance',
-                                                                        'uwProcesses'           => 'Processes'
+                                                                        'uwProcesses'           => 'Processes',
                                                                         'uwProcessesNew'        => 'New Submission Review Process',
                                                                         'uwProcessesCondition'  => 'Condition Review Process',
                                                                         'uwSystems'             => 'Systems',
