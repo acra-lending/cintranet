a496a060a100c93cd3a23d3853286109a6d4d1b1
Chris <chrisliu0416@gmail.com>
2021-10-01 11:55:17 -0700

    Update Benefit Effective Date Calculator

--- public/js/pages/benefitdatecalculator.js
+++ public/js/pages/benefitdatecalculator.js
@@ -10,10 +10,3 @@ const calcDate = () => {
     const firstOfMonth = new Date(effectiveMonth.setMonth(date.getMonth()+1,1));
     document.getElementById('calculatedDate').value = firstOfMonth.toLocaleDateString();
 };
-
-$("document").ready(function(){
-    setTimeout(function(){
-       $("div.alert").remove();
-    }, 5000 ); // 5 secs
-
-});
