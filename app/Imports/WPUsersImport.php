0c2d92e31ff168675806062e4f87ce9f996903ec
Chris <chrisliu0416@gmail.com>
2021-10-27 11:09:39 -0700

    Excel import data validation to skip empty cells

--- app/Imports/WPUsersImport.php
+++ app/Imports/WPUsersImport.php
@@ -18,59 +18,64 @@ class WPUsersImport implements ToCollection
     */
     public function collection(Collection $rows)
     {
-        foreach ($rows as $row)
+        foreach ($rows->slice(1) as $row)
         {
-            $remove[] = "'";
-            $remove[] = " ";
-            $remove[] = "-";
-            
-            $firstName = $row[0];
-            $lastName = $row[1];
-            $emailAddress = strtolower($row[2]);
-            $lastNameWithNoSpace = str_replace($remove, "", $lastName);
-            $userName = strtolower($lastNameWithNoSpace .$firstName[0] .time());
-            $tempPassword = $lastNameWithNoSpace.'$1!';
-
-            $token = Http::post('https://acralending.com/wp-json/jwt-auth/v1/token', [
-                'username' => env('WP_USERNAME'),
-                'password' => env('WP_PASSWORD'),
-            ]);
-    
-            $token = $token->json()['token'];
-    
-            $response = Http::withToken($token)
-            ->post('https://acralending.com/wp-json/wp/v2/users', [
-                    'username' => $userName,
-                    'email' => $emailAddress,
-                    'first_name' => $firstName,
-                    'last_name' => $lastName,
-                    'roles' => 'um_broker',
-                    'password' => $tempPassword,
-            ]);
-
-            $data = [
-                [
-                    'firstname'     => $firstName,
-                    'lastname'      => $lastName,
-                    'email'         => $emailAddress,
-                ],
-                'username'      => $userName,
-                'tempPassword'  => $tempPassword,
-            ];
-
-            $emailArray = array('webupdates@acralending.com', 'brokerportalconfirmations@citadelservicing.com'); 
-
-            if ($response->successful()) {
-                Mail::to($emailArray)
-                ->queue(new BrokerPortalRequests($data));
-                Mail::to([
-                    $emailAddress
-                ])->queue(new BrokerPortalRequestsClient($data));
+            if($row->filter()->isNotEmpty()){
+                $remove[] = "'";
+                $remove[] = " ";
+                $remove[] = "-";
+                
+                $firstName = $row[0];
+                $lastName = $row[1];
+                $emailAddress = strtolower($row[2]);
+                $lastNameWithNoSpace = str_replace($remove, "", $lastName);
+                // dd($lastNameWithNoSpace);
+                $userName = strtolower($lastNameWithNoSpace .$firstName[0] .time());
+                $tempPassword = $lastNameWithNoSpace.'$1!';
         
-                // return redirect ('/usermanagement/wp-users')->with ('success', 'Credentials created');
+                $token = Http::post('https://acralending.com/wp-json/jwt-auth/v1/token', [
+                    'username' => env('WP_USERNAME'),
+                    'password' => env('WP_PASSWORD'),
+                ]);
+        
+                $token = $token->json()['token'];
+        
+                $response = Http::withToken($token)
+                ->post('https://acralending.com/wp-json/wp/v2/users', [
+                        'username' => $userName,
+                        'email' => $emailAddress,
+                        'first_name' => $firstName,
+                        'last_name' => $lastName,
+                        'roles' => 'um_broker',
+                        'password' => $tempPassword,
+                ]);
+        
+                $data = [
+                    [
+                        'firstname'     => $firstName,
+                        'lastname'      => $lastName,
+                        'email'         => $emailAddress,
+                    ],
+                    'username'      => $userName,
+                    'tempPassword'  => $tempPassword,
+                ];
+        
+                $emailArray = array('webupdates@acralending.com', 'brokerportalconfirmations@citadelservicing.com'); 
+        
+                if ($response->successful()) {
+                    Mail::to($emailArray)
+                    ->queue(new BrokerPortalRequests($data));
+                    Mail::to([
+                        $emailAddress
+                    ])->queue(new BrokerPortalRequestsClient($data));
+            
+                    // return redirect ('/usermanagement/wp-users')->with ('success', 'Credentials created');
+                }
+                else 
+                $message = $response->json()['message'];
+
             }
-            else 
-            $message = $response->json()['message'];
+
         }
 
         if ($response->successful()) {
