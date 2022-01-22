d73c63c22934b137cd985ad3a600a58b03314d2f
Chris <chrisliu0416@gmail.com>
2022-01-21 16:19:04 -0800

    Remove dashboard banner

--- resources/views/pages/dashboard/welcome.blade.php
+++ resources/views/pages/dashboard/welcome.blade.php
@@ -1,10 +1,10 @@
-<div class="card">
-    <img class ="img-fluid rounded" src="{{ asset('img/dashboard-bg.jpg') }}">
-      <h3 class="text-light dashboard-h3" style="justify-content: center">Welcome to Cintranet, {{ $first_name }}!</h3>
+<div class="mb-3">
+    {{-- <img class ="img-fluid rounded" src="{{ asset('img/dashboard-bg.jpg') }}"> --}}
+      <h3 class="text-black " style="justify-content: center">Welcome to Cintranet, {{ $first_name }}!</h3>
   </div>
   <div class="container">
   <!-- SEARCH FORM -->  
-  <form action="/search" method="GET" role="search">
+  {{-- <form action="/search" method="GET" role="search">
     {{ csrf_field() }}
     <div class="input-group input-group dashboard-input" style="width:75%;">
     <input class="form-control form-control-navbar" type="search" name="q" placeholder="Search Files, Posts, Events, or Users" aria-label="Search">
@@ -14,5 +14,5 @@
         </button>
     </div>
     </div>
-  </form>
+  </form> --}}
 </div>
