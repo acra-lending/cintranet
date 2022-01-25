b599b99b8b31ea0298f7cc2271617084415304d3
Chris <chrisliu0416@gmail.com>
2022-01-25 08:36:29 -0800

    Add margin under Welcome message

--- resources/views/pages/dashboard/welcome.blade.php
+++ resources/views/pages/dashboard/welcome.blade.php
@@ -1,11 +1,11 @@
-<div class="mb-3">
-  <h3 class="text-black " style="justify-content: center">Welcome to Cintranet, {{ $first_name }}!</h3>
+<div>
+  <h3 class="text-black mb-3" style="justify-content: center">Welcome to Cintranet, {{ $first_name }}!</h3>
     <img class ="img-fluid rounded" src="{{ asset('img/dashboard-bg.jpg') }}">
   </div>
   <div class="container">
   <!-- SEARCH FORM -->  
   <form action="/search" method="GET" role="search">
-    {{ csrf_field() }}
+    @csrf
     <div class="input-group input-group dashboard-input" style="width:75%;">
     <input class="form-control form-control-navbar" type="search" name="q" placeholder="Search Files, Posts, Events, or Users" aria-label="Search">
     <div class="input-group-append">
