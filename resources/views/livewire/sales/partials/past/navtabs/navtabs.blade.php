f2244efc59d3cc7d4238c1ec408c5739bdfc33d1
Chris Liu <chrisl@CSC.local>
2022-06-14 15:24:32 -0700

    Fix typo in Past Rate sheets

--- resources/views/livewire/sales/partials/past/navtabs/navtabs.blade.php
+++ resources/views/livewire/sales/partials/past/navtabs/navtabs.blade.php
@@ -2,7 +2,7 @@
     <a class="nav-link active" id="vert-tabs-ombs-tab_past" data-toggle="pill" href="#vert-tabs-ombs_past" role="tab" aria-controls="vert-tabs-ombs_past" aria-selected="true" wire:ignore>OMBS & VOE</a>
     <a class="nav-link" id="vert-tabs-3mbs-tab_past" data-toggle="pill" href="#vert-tabs-3mbs_past" role="tab" aria-controls="vert-tabs-3mbs_past" aria-selected="false" wire:ignore>3MBS</a>
     <a class="nav-link" id="vert-tabs-dscr-tab_past" data-toggle="pill" href="#vert-tabs-dscr_past" role="tab" aria-controls="vert-tabs-dscr_past" aria-selected="false" wire:ignore>Business Purpose / DSCR</a>
-    <a class="nav-link" id="vert-tabs-itin-tab_past" data-toggle="pill" href="#vert-tabs-itin_past" role="tab" aria-controls="vert-tabs-itin_past" aria-selected="false" wire:ignore>Business Purpose / DSCR</a>
+    <a class="nav-link" id="vert-tabs-itin-tab_past" data-toggle="pill" href="#vert-tabs-itin_past" role="tab" aria-controls="vert-tabs-itin_past" aria-selected="false" wire:ignore>ITIN</a>
     <a class="nav-link" id="vert-tabs-non-prime-ws-tab_past" data-toggle="pill" href="#vert-tabs-non-prime-ws_past" role="tab" aria-controls="vert-tabs-non-prime-ws_past" aria-selected="false" wire:ignore>Non-Prime Wholesale</a>
     <a class="nav-link" id="vert-tabs-jumbo-prime-ws-tab_past" data-toggle="pill" href="#vert-tabs-jumbo-prime-ws_past" role="tab" aria-controls="vert-tabs-jumbo-prime-ws_past" aria-selected="false" wire:ignore>Jumbo Prime Wholesale</a>
     <a class="nav-link" id="vert-tabs-small-balance-multifamily-tab_past" data-toggle="pill" href="#vert-tabs-small-balance-multifamily_past" role="tab" aria-controls="vert-tabs-small-balance-multifamily_past" aria-selected="false" wire:ignore>Small Balance Multifamily</a>
