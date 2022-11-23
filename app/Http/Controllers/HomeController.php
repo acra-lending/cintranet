99695654b5cd2536db570ced9635b9781c773ab9
Chris <chrisliu0416@gmail.com>
2022-11-23 08:34:10 -0800

    Remove users with softdelete from teamMembers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,6 +79,7 @@ class HomeController extends Controller
             ->value('team');  
 
             $teamMembers = DB::table('s2zar_jsn_users')
+            ->where('s2zar_jsn_users.deleted_at', NULL)
             ->join('s2zar_users', 's2zar_users.id', 's2zar_jsn_users.id')
             ->where('team', $team)
             ->orWhere('s2zar_users.name', $team)
