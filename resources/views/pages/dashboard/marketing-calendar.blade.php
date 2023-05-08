735d656e27102d5a45078707a5772aa3f754a08c
Matthew Kirby <Matthew.Kirby@CSC.local>
2023-05-07 22:15:00 -0700

    added heading to marketing calendar

--- resources/views/pages/dashboard/marketing-calendar.blade.php
+++ resources/views/pages/dashboard/marketing-calendar.blade.php
@@ -1,3 +1,10 @@
+<div class="card card-danger card-outline">
+    <div class="card-header">
+    <h3 class="card-title">
+        <i class="nav-icon fa fa-calendar"></i>
+        Marketing Calendar
+    </h3>
+    </div>
 <div style="border-top: 3px solid #0033a1; border-radius: 6px; overflow: hidden; margin: 15px auto; max-width: 736px;">
     <iframe scrolling="no" src="https://acralending.com/marketing-calendar/" style="border: 0px none; margin-left: -185px; height: 229px; margin-top: -115px; width: 926px;">
     </iframe>
