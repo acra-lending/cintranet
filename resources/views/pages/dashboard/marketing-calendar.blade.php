b3da06fa30e1f4bfc65f325ad60185d99310d82f
Matthew Kirby <Matthew.Kirby@CSC.local>
2023-05-07 22:23:07 -0700

    resized iframe

--- resources/views/pages/dashboard/marketing-calendar.blade.php
+++ resources/views/pages/dashboard/marketing-calendar.blade.php
@@ -5,7 +5,7 @@
         Marketing Calendar
     </h3>
     </div>
-<div style="border-top: 3px solid #0033a1; border-radius: 6px; overflow: hidden; margin: 15px auto; max-width: 736px;">
+<div style="overflow: hidden; margin: 15px auto; max-width: 33.333333%;">
     <iframe scrolling="no" src="https://acralending.com/marketing-calendar/" style="border: 0px none; margin-left: -185px; height: 229px; margin-top: -115px; width: 926px;">
     </iframe>
 </div>
