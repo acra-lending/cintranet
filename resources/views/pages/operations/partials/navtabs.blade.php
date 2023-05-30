586f2a9b8d84755be842b895cbf683737ce88377
Matthew Kirby <Matthew.Kirby@CSC.local>
2023-05-30 13:59:01 -0700

    fixing errors

--- resources/views/pages/operations/partials/navtabs.blade.php
+++ resources/views/pages/operations/partials/navtabs.blade.php
@@ -9,7 +9,7 @@
     <a class="nav-item nav-link" id="nav-lsu-tab" data-toggle="tab" href="#nav-lsu" role="tab" aria-controls="nav-lsu" aria-selected="false">Loan Set Up</a>
     <a class="nav-item nav-link" id="nav-tm-tab" data-toggle="tab" href="#nav-tm" role="tab" aria-controls="nav-tm" aria-selected="false">Transaction Manager</a>
     <a class="nav-item nav-link" id="nav-uw-tab" data-toggle="tab" href="#nav-uw" role="tab" aria-controls="nav-uw" aria-selected="false">Underwriting</a>
-    <!-- <a class="nav-item nav-link" id="nav-valuations-tab" data-toggle="tab" href="#nav-valuations" role="tab" aria-controls="nav-valuations" aria-selected="false">Valuation Reviews</a> -->
+    <a class="nav-item nav-link" id="nav-valuations-tab" data-toggle="tab" href="#nav-valuations" role="tab" aria-controls="nav-valuations" aria-selected="false">Valuation Waterfall</a>
     <a class="nav-item nav-link" id="nav-jr-processor-tab" data-toggle="tab" href="#nav-jr-processor" role="tab" aria-controls="nav-jr-processor" aria-selected="false">Jr Processor</a>
     <a class="nav-item nav-link" id="nav-processor-tab" data-toggle="tab" href="#nav-processor" role="tab" aria-controls="nav-processor" aria-selected="false">Processor</a>
     <a class="nav-item nav-link" id="nav-nda-tab" data-toggle="tab" href="#nav-nda" role="tab" aria-controls="nav-nda" aria-selected="false">NDA</a>
